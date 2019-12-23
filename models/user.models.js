68beed4c2249b7ee294b38a817098c3d12005e93
kaila52 <thuan17111999@gmail.com>
2019-12-23 21:22:15 +0700

    pull heroku

--- models/user.models.js
+++ models/user.models.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://dbAadmin:12345@cluster0-rh8vg.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URI||'mongodb+srv://dbAadmin:12345@cluster0-rh8vg.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 const userSchema = new mongoose.Schema({
     fullname:String,
     user:String,
